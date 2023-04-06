dc9ffabfe2cbb2faec210d0930af85495e0753b7
djmerr3 <djmerr@iu.edu>
2023-04-05 20:34:48 -0400

    db connection

---
+++ database.class.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Author: DJ Merrell
+ * Date: 4/5/23
+ * File: database.class.php
+ * Description:
+ */
+
+
+/* Author: Louie Zhu
+ * Date: 10/20/2021
+ * Name: database.class.php
+ * Description: the Database class sets the database details.
+ */
+
+class Database
+{
+
+    //define database parameters
+    private $param = array(
+        'host' => 'localhost',
+        'login' => 'root',
+        'password' => '',
+        'database' => 'finalproject_db',
+        'tblUser' => 'users'
+    );
+    //define the database connection object
+    private $objDBConnection = NULL;
+    static private $_instance = NULL;
+
+    //constructor
+    public function __construct()
+    {
+
+        $this->objDBConnection = @new mysqli(
+            $this->param['host'],
+            $this->param['login'],
+            $this->param['password'],
+            $this->param['database']
+        );
+        if (mysqli_connect_errno() != 0) {
+            exit("Connecting to database failed: " . mysqli_connect_error());
+        }
+    }
+
+    //static method to ensure there is just one Database instance
+    static public function getInstance()
+    {
+        if (self::$_instance == NULL) {
+            self::$_instance = new Database();
+        }
+        return self::$_instance;
+    }
+
+    //this function returns the database connection object
+    public function getConnection()
+    {
+        return $this->objDBConnection;
+    }
+
+    //returns the name of the table storing books
+    public function getUserTable()
+    {
+        return $this->param['tblUser'];
+    }
+}
